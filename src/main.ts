b9583860913a7e8aaaa9d1692940a76a4d8bd5a6
José Luis Palillero Huerta <jose.palillero.ext@kavak.com>
2022-04-04 15:59:40 -0500

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(PORT);
 }
 bootstrap();
